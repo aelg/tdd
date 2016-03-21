c305599523f5af83e1078400af3340b262996af3
Linus Mellberg <linus.mellberg@infinera.com>
2016-03-21 12:23:01 +0100

    Example solution.

--- roman.c
+++ roman.c
@@ -6,5 +6,30 @@ Author     : Linus Mellberg
 */
 
 void number_to_roman_numeral(int number, char * numeral){
-    numeral[0] = '\0';
+    const int thresholds[] = {1000, 500, 100, 50, 10, 5, 1, 0};
+    const char numerals[] = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+
+    const int correctedThresholds[] = {900, 400, 90, 40, 9, 4, 0};
+    const char correctedNumerals[] = {'C', 'C', 'X', 'X', 'I', 'I'};
+
+    int pos = 0;
+    while(number > 0) {
+        int index;
+        for(index = 0; thresholds[index] != 0; ++index){
+            int currentThreshold = thresholds[index];
+            if(number >= currentThreshold){
+                numeral[pos++] = numerals[index];
+                number = number - currentThreshold;
+                break;
+            }
+            int currentCorrectedThreshold = correctedThresholds[index];
+            if(number >= currentCorrectedThreshold){
+                numeral[pos++] = correctedNumerals[index];
+                numeral[pos++] = numerals[index];
+                number = number - currentCorrectedThreshold;
+                break;
+            }
+        }
+    }
+    numeral[pos] = '\0';
 }
--- test/roman_test.cc
+++ test/roman_test.cc
@@ -21,3 +21,128 @@ TEST(RomanNumeral, canConvert1){
     number_to_roman_numeral(1, numeral);
     EXPECT_STREQ(numeral, "I");
 }
+
+TEST(RomanNumeral, canConvert2){
+    number_to_roman_numeral(2, numeral);
+    EXPECT_STREQ(numeral, "II");
+}
+
+TEST(RomanNumeral, canConvert3){
+    number_to_roman_numeral(3, numeral);
+    EXPECT_STREQ(numeral, "III");
+}
+
+TEST(RomanNumeral, canConvert4){
+    number_to_roman_numeral(4, numeral);
+    EXPECT_STREQ(numeral, "IV");
+}
+
+TEST(RomanNumeral, canConvert5){
+    number_to_roman_numeral(5, numeral);
+    EXPECT_STREQ(numeral, "V");
+}
+
+TEST(RomanNumeral, canConvert9){
+    number_to_roman_numeral(9, numeral);
+    EXPECT_STREQ(numeral, "IX");
+}
+
+TEST(RomanNumeral, canConvert10){
+    number_to_roman_numeral(10, numeral);
+    EXPECT_STREQ(numeral, "X");
+}
+
+TEST(RomanNumeral, canConvert11){
+    number_to_roman_numeral(11, numeral);
+    EXPECT_STREQ(numeral, "XI");
+}
+
+TEST(RomanNumeral, canConvert24){
+    number_to_roman_numeral(24, numeral);
+    EXPECT_STREQ(numeral, "XXIV");
+}
+
+TEST(RomanNumeral, canConvert41){
+    number_to_roman_numeral(41, numeral);
+    EXPECT_STREQ(numeral, "XLI");
+}
+
+TEST(RomanNumeral, canConvert49){
+    number_to_roman_numeral(49, numeral);
+    EXPECT_STREQ(numeral, "XLIX");
+}
+
+TEST(RomanNumeral, canConvert50){
+    number_to_roman_numeral(50, numeral);
+    EXPECT_STREQ(numeral, "L");
+}
+
+TEST(RomanNumeral, canConvert99){
+    number_to_roman_numeral(99, numeral);
+    EXPECT_STREQ(numeral, "XCIX");
+}
+
+TEST(RomanNumeral, canConvert100){
+    number_to_roman_numeral(100, numeral);
+    EXPECT_STREQ(numeral, "C");
+}
+
+TEST(RomanNumeral, canConvert101){
+    number_to_roman_numeral(101, numeral);
+    EXPECT_STREQ(numeral, "CI");
+}
+
+TEST(RomanNumeral, canConvert109){
+    number_to_roman_numeral(109, numeral);
+    EXPECT_STREQ(numeral, "CIX");
+}
+
+TEST(RomanNumeral, canConvert349){
+    number_to_roman_numeral(349, numeral);
+    EXPECT_STREQ(numeral, "CCCXLIX");
+}
+
+TEST(RomanNumeral, canConvert449){
+    number_to_roman_numeral(449, numeral);
+    EXPECT_STREQ(numeral, "CDXLIX");
+}
+
+TEST(RomanNumeral, canConvert500){
+    number_to_roman_numeral(500, numeral);
+    EXPECT_STREQ(numeral, "D");
+}
+
+TEST(RomanNumeral, canConvert509){
+    number_to_roman_numeral(509, numeral);
+    EXPECT_STREQ(numeral, "DIX");
+}
+
+TEST(RomanNumeral, canConvert944){
+    number_to_roman_numeral(944, numeral);
+    EXPECT_STREQ(numeral, "CMXLIV");
+}
+
+TEST(RomanNumeral, canConvert999){
+    number_to_roman_numeral(999, numeral);
+    EXPECT_STREQ(numeral, "CMXCIX");
+}
+
+TEST(RomanNumeral, canConvert1999){
+    number_to_roman_numeral(1999, numeral);
+    EXPECT_STREQ(numeral, "MCMXCIX");
+}
+
+TEST(RomanNumeral, canConvert2016){
+    number_to_roman_numeral(2016, numeral);
+    EXPECT_STREQ(numeral, "MMXVI");
+}
+
+TEST(RomanNumeral, canConvert3999){
+    number_to_roman_numeral(3999, numeral);
+    EXPECT_STREQ(numeral, "MMMCMXCIX");
+}
+
+TEST(RomanNumeral, canConvert4000){
+    number_to_roman_numeral(4000, numeral);
+    EXPECT_STREQ(numeral, "MMMM");
+}
